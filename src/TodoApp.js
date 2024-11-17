f4067d3584b4fb3fff450cd903f3d2b8f94d310a
Evan Parker <65721884+evancp87@users.noreply.github.com>
2024-11-17 16:19:38 +0000

    feat: sass implemented

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,6 @@
-import React from "react";
-import TodoList from "./component/TodoList";
-import "./styles.css";
+import React from 'react';
+import TodoList from './component/TodoList';
+import './styles/app.css';
 
 export default function TodoApp() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
 
-import TodoApp from "./TodoApp";
+import TodoApp from './TodoApp';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <TodoApp />
