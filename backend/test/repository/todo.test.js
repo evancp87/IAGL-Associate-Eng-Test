7ddad64a8f5e4e2f59132ca0b035c437f6b29117
Evan Parker <65721884+evancp87@users.noreply.github.com>
2024-11-17 15:30:04 +0000

    test: add todo test tdd

--- backend/test/repository/todo.test.js
+++ backend/test/repository/todo.test.js
@@ -1,14 +1,27 @@
-
 const repository = require('../../src/repository/todo');
 
 describe('TODO repository', () => {
   it('should return the todo list', async () => {
     const expected = {
-      todos: [{
-        "task": "This is a todo example"
-      }]
+      todos: [
+        {
+          task: 'This is a todo example',
+        },
+      ],
     };
     const actual = await repository.getTodos();
     expect(actual).toEqual(expected);
-  });
+  }),
+    it('should be able to add a new todo to the todolist', async () => {
+      const { todos } = await repository.getTodos();
+
+      const newTodo = { task: 'Hi, this is a new todo!' };
+
+      todos.push(newTodo);
+
+      const actual = await repository.getTodos();
+
+      expect(actual.todos).toHaveLength(2);
+      expect(actual.todos[1]).toEqual(newTodo);
+    });
 });
